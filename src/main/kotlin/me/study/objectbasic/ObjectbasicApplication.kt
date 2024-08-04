641e399a58e7378d30c1d17ccee258daf4923441
geneaky <kaktus418@gmail.com>
2024-08-05 07:49:57 +0900

    first commit

---
+++ src/main/kotlin/me/study/objectbasic/ObjectbasicApplication.kt
@@ -0,0 +1,11 @@
+package me.study.objectbasic
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class ObjectbasicApplication
+
+fun main(args: Array<String>) {
+    runApplication<ObjectbasicApplication>(*args)
+}
---
+++ src/test/kotlin/me/study/objectbasic/ObjectbasicApplicationTests.kt
@@ -0,0 +1,13 @@
+package me.study.objectbasic
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class ObjectbasicApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
